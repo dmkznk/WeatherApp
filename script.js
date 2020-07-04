8a4c781b071e7d098717b4b4f98b03fd57973d04
Dima Kuzenko <dimas@Dimas-MacBook-Pro.local>
2020-07-04 19:54:50 +0300

    geolocation added

--- script.js
+++ script.js
@@ -1,36 +1,73 @@
 const getData = async value => {
   try {
+    if (!value) return
     let apiKey = "19b6f658c03014c2930d23477b161226"
     let responce = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}`
+      // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
     )
     let result = await responce.json()
-    insertData(result)
+    render(result, null)
   } catch (error) {
-    document.body.innerHTML = error
+    render(null, error)
   }
 }
 
-const insertData = data => {
-  const weatherInfo = document.querySelector(".weather_info")
-  weatherInfo.innerHTML = `
-             <img src="https://openweathermap.org/img/wn/${
-               data.weather[0].icon
-             }@2x.png"alt="img">
-            <span class="city">${data.name}</span>
-            <span class="temperature">${Math.round(
-              data.main.temp - 273
-            )}&deg;</span>
-            <span class="sky">${data.weather[0].main}</span>
-  `
+const sucessfulLookup = async position => {
+  try {
+    const {
+      latitude,
+      longitude
+    } = position.coords
+
+    let responce = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=b819976a24274eb4ab3d638cea23f92e`)
+    let result = await responce.json()
+
+    const userCity = result.results[0].components.city
+    getData(userCity)
+
+  } catch (error) {
+    render(null, error)
+  }
 }
 
+const render = (data, error) => {
+  const weatherInfo = document.querySelector(".weather_info")
+  if (error) {
+    weatherInfo.innerHTML = `<span class="error">${error}</span>`
+  } else {
+    weatherInfo.innerHTML = `
+  <img src="https://openweathermap.org/img/wn/${
+    data.weather[0].icon
+  }@2x.png"alt="img">
+ <span class="city">${data.name}</span>
+ <span class="temperature">${Math.round(
+   data.main.temp - 273
+ )}&deg;</span>
+ <span class="sky">${data.weather[0].main}</span>
+`
+  }
+}
+
+
 const chooseCity = () => {
-  const cities = document.querySelector(".cities")
-  getData(cities[1].value)
-  cities.addEventListener("change", event => {
-    getData(event.target.value)
+  const input = document.querySelector('input')
+  getData(input.value)
+  input.value = ''
+}
+
+const trigger = () => {
+  const btnSubmit = document.querySelector('button')
+  btnSubmit.addEventListener('click', () => {
+    chooseCity()
+  })
+
+  document.addEventListener('keydown', e => {
+    if (e.key === 'Enter') chooseCity()
   })
 }
 
-chooseCity()
+geo = window.navigator.geolocation
+geo.getCurrentPosition(sucessfulLookup)
+
+trigger()
